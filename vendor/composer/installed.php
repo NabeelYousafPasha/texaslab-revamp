889c374ea2b86038550ad08a9240023f18522f8a
arxlanakram4040 <arxlanakram4040@gmail.com>
2023-12-09 01:33:36 +0500

    added gitignore

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b52eb3437ae974b24df16744258c086b18e2f375',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -407,9 +407,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b52eb3437ae974b24df16744258c086b18e2f375',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
