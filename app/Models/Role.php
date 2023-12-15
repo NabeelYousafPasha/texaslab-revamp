2112d836ee401bcad8f86d80950a26762c502776
arxlanakram4040 <arxlanakram4040@gmail.com>
2023-12-15 22:38:34 +0500

    TXL: refactored user.php and role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,11 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{
+    Builder,
+    Factories\HasFactory
+};
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
     use HasFactory;
+
+    const SUPER_ADMIN = 'super_admin';
+
+    /**
+     *
+     * @return array
+     */
+    public static function getAllRoleConstants(): array
+    {
+        return [
+            self::SUPER_ADMIN,
+        ];
+    }
+
+    /**
+     * |--------------------------------------------------------------------------
+     * | SCOPES
+     * |--------------------------------------------------------------------------
+     */
+
+    /**
+     * @param Builder $query
+     *
+     * @return Builder
+     */
+    public function scopeOfSuperAdmin(Builder $query): Builder
+    {
+        return $query->where('name', '=', self::SUPER_ADMIN);
+    }
 }
